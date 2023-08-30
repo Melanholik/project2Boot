785814c3fac765ce5c0946061cec3e25bb232858
Melanholik <Forliho@gmail.com>
2023-08-30 14:45:32 +0300

    Add CSRF

--- springSecurity/src/main/java/by/melanholik/demo/config/SecurityConfig.java
+++ springSecurity/src/main/java/by/melanholik/demo/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // конфигурируем сам Spring Security
         // конфигурируем авторизацию
-        http.csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
